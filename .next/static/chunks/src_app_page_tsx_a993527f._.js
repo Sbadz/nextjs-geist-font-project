357acf0a22db5b6d84965349486028c1cbd98797
Sbadz <sbadtothebone@gmail.com>
2025-07-15 09:27:55 -0400

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752586075577

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c977f507._.js",
+  "static/chunks/node_modules_bd5f1d55._.js"
+],
+    source: "dynamic"
+});
